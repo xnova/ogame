c31fcdb764da53b12f168b2fff8d3e359c69fd3d
arkeros <rafael@arque.ro>
2017-09-14 13:56:55 +0200

    restart code

--- index.android.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-import React, {
-  AppRegistry,
-} from 'react-native';
-import OGame from './app';
-
-AppRegistry.registerComponent('OGame', () => OGame);
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import runtime from './runtime';
-import intl from './intl';
-
-export default combineReducers({
-  runtime,
-  intl,
-});
